52e4ff97970b1f64445c7c27020ceeb8f8d54580
mandresoeiro <mandre.soeiro@gmail.com>
2024-10-15 11:30:36 -0300

    first commit

---
+++ SchoolProject/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, re_path
+from StudentApp import views
+
+urlpatterns = [
+    re_path(r'^student$', views.studentApi),
+    re_path(r'^student$', views.studentApi),
+    re_path(r'^student/([0-9]+)$', views.studentApi),
+    path('admin/', admin.site.urls),
+]
